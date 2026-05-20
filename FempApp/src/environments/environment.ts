25948c9b20cb852e56cbea769e524089e2e3f5a8
Luis Gutierrez <luisoguti09@gmail.com>
2026-05-20 19:09:18 -0300

    environment fixed

--- FempApp/src/environments/environment.ts
+++ FempApp/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    SERVER_API: "https://fempapp-back-production.up.railway.app",
+    SERVER_API: "https://fempapp-back-develop.up.railway.app",
     production: false,
     googleMapsApiKey: "AIzaSyDbYqUVDgKvj2RoynplKO_fg0BCMZLVmdc"
 };
