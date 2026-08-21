316b55e382d1f414c417d5ceb83ade49fc6bcf9d
Luis Gutierrez <luisoguti09@gmail.com>
2026-08-20 22:00:28 -0300

    Ajustar UsuarioEventos para inscripcion por perfil deportivo

--- FempApp-bck/models/usuarioevento.model.js
+++ FempApp-bck/models/usuarioevento.model.js
@@ -2,19 +2,57 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
 const UsuarioEventos = sequelize.define('UsuarioEventos', {
-  EventoId: { type: DataTypes.INTEGER, allowNull: false, primaryKey: true, field: 'EventoId' },
-  UsuarioId: { type: DataTypes.INTEGER, allowNull: false, primaryKey: true, field: 'UsuarioId' },
-  perfilDeportivoId: { type: DataTypes.INTEGER, allowNull: true },
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
 
-  // campos opcionales adicionales a la inscripción
-  categoria: { type: DataTypes.STRING(80), allowNull: true },
-  disciplina: { type: DataTypes.STRING(80), allowNull: true },
-  division: { type: DataTypes.STRING(20), allowNull: true },
-  grupo: { type: DataTypes.STRING(50), allowNull: true },
-  rol: { type: DataTypes.ENUM('deportista', 'coach', 'juez', 'staff', 'otro'), allowNull: true },
+  EventoId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    field: 'EventoId'
+  },
+
+  UsuarioId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    field: 'UsuarioId'
+  },
+
+  perfilDeportivoId: {
+    type: DataTypes.INTEGER,
+    allowNull: true
+  },
+
+  categoria: {
+    type: DataTypes.STRING(255),
+    allowNull: true
+  },
+
+  disciplina: {
+    type: DataTypes.STRING(255),
+    allowNull: true
+  },
+
+  division: {
+    type: DataTypes.STRING(255),
+    allowNull: true
+  },
+
+  grupo: {
+    type: DataTypes.STRING(255),
+    allowNull: true
+  },
+
+  rol: {
+    type: DataTypes.STRING(50),
+    allowNull: true,
+    defaultValue: 'deportista'
+  }
 }, {
   tableName: 'usuarioeventos',
-  timestamps: true,
+  timestamps: true
 });
 
 module.exports = UsuarioEventos;
